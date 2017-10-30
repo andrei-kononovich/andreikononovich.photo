93202c24dfa3fa1472df3575b02df5f4316ec34e
Yauheni_Sauchanka <Yauheni_Sauchanka@epam.com>
2017-10-30 16:24:02 +0300

    express

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # andreikononovich-photo
 businesscard website for photographer
 
+```$xslt
+$ npm install
+$ bower install
+```
+
 2643195@gmail.com
 
 tel. +48 574 665 316
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const app = express();
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
 require('dotenv').config();
+
+
+const app = express();
 
 const port = process.env.PORT || 80;
 
-app.use(express.static('./'));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/aboutme', (req, res) => res.render('aboutme'));
+app.get('/contacts', (req, res) => res.render('contacts'));
+app.get('/albums/:id', (req, res) => res.render('album'));
 
 // start the server
 app.listen(port);
